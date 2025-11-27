5b153fbed0bea61ac0eba16f95806a36ec1a7b18
Ayan <sujon.seo.bd@gmail.com>
2025-11-27 14:23:17 +0600

    Payment confirmation api added

--- index.js
+++ index.js
@@ -114,7 +114,7 @@ async function run() {
         metadata: {
           parcelId: paymentInfo.parcelId 
         },
-        success_url: `${process.env.SITE_DOMAIN}/dashboard/payment-success`,
+        success_url: `${process.env.SITE_DOMAIN}/dashboard/payment-success?session_id={CHECKOUT_SESSION_ID}`,
         cancel_url: `${process.env.SITE_DOMAIN}/dashboard/payment-cancelled`,
       })
 
@@ -122,8 +122,60 @@ async function run() {
       res.send({url: session.url});
     })
 
+    // old 
+    // // Payment Related Apis 
+    // app.post('/create-checkout-session', async (req, res) => {
+    //   const paymentInfo = req.body;
+    //   const amount = parseInt(paymentInfo.cost) * 100;
+    //   const session = await stripe.checkout.sessions.create({
+    //     line_items: [
+    //       {
+          
+    //         price_data: {
+    //           currency: 'USD',
+    //           unit_amount: amount,
+    //           product_data: {
+    //             name: paymentInfo.parcelName
+    //           }
+    //         },
+           
+    //         quantity: 1,
+    //       },
+    //     ],
+    //      customer_email: paymentInfo.senderEmail, 
+    //     mode: 'payment',
+    //     metadata: {
+    //       parcelId: paymentInfo.parcelId 
+    //     },
+    //     success_url: `${process.env.SITE_DOMAIN}/dashboard/payment-success`,
+    //     cancel_url: `${process.env.SITE_DOMAIN}/dashboard/payment-cancelled`,
+    //   })
 
+    //   console.log(session);
+    //   res.send({url: session.url});
+    // })
 
+// Payment confirmation api 
+app.patch(`/payment-success`, async(req, res)=> {
+  const sessionId = req.query.session_id;
+
+  const session = await stripe.checkout.sessions.retrieve(sessionId);
+  console.log('session retrive: ', session);
+  if(session.payment_status === 'paid'){
+    const id = session.metadata.parcelId;
+    const query = {_id: new ObjectId(id)}
+    const update = {
+      $set: {
+        paymentStatus: 'paid',
+
+      }
+    }
+    const result = await parcelsCollections.updateOne(query, update);
+    res.send(result);
+  }
+  // console.log('session id: ', sessionId);
+  res.send({success: false})
+})
 
 
 
